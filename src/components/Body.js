ccaddb61b6c0fd057833b46af2368754eab75ddb
Anushi Jindal <jindala982@gmail.com>
2024-05-25 14:00:51 +0530

    next commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,8 +13,8 @@ const Body=()=>{
     const fetchData=async () => {
         const data1=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json1=await data1.json();
-        setlistOfRes(json1?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setnameFilter(json1?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setlistOfRes(json1?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setnameFilter(json1?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
     useEffect(()=>{
         fetchData();
@@ -50,7 +50,7 @@ const Body=()=>{
             }>Top Rated Restaurants</button>
             </div>
             </div>
-            <div className="flex flex-wrap justify-between">
+            <div className="grid grid-cols-5 h-[100%] justify-between px-5">
                 {nameFilter.map((restaurant)=>
                 (
                     <Link key={restaurant.info.id} to={"/restaurant/"+restaurant.info.id}>
